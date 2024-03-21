8bdbba2f0a6ed5dfbdd6e80fa5adab37943bbb45
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2024-03-21 22:30:10 +0900

    Update code

--- mdl_comp.py
+++ mdl_comp.py
@@ -18,7 +18,7 @@ def prac_mdl_comp(X_train, y_train, variance=1):
         eigensum = 0.5 * np.sum(np.log((eigenvals + l) / l))
         return (mse_norm + theta_norm + eigensum) / y_train.size
 
-    opt_solved = scipy.optimize.minimize(prac_mdl_comp_objective, x0=1e-10)
+    opt_solved = scipy.optimize.minimize(prac_mdl_comp_objective, bounds=((0.0, np.inf),), x0=1e-10)
     prac_mdl = opt_solved.fun
     lambda_opt = opt_solved.x
     thetahat = calc_thetahat(lambda_opt)
@@ -28,4 +28,9 @@ def prac_mdl_comp(X_train, y_train, variance=1):
         'lambda_opt': lambda_opt,
         'thetahat': thetahat
     }
+
+def redundancy(X_train, l):
+    eigenvals, eigenvecs = np.linalg.eig(X_train.T @ X_train)
+    eigensum = 0.5 * np.sum(np.log((eigenvals + l) / l))
+    return eigensum
 
