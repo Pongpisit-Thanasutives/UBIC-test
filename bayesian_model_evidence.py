3680f0ba6b4d2f665d9ff7a1552bdb02e1330b74
Pongpisit Thanasutives <pongpisit.tha@gmail.com>
2025-05-14 12:19:17 +0900

    Update

--- bayesian_model_evidence.py
+++ bayesian_model_evidence.py
@@ -1,13 +1,15 @@
 import numpy as np
 from scipy.special import loggamma
 
-def log_evidence(X_full, y, effective_indices, v=1, standardize=False):
-    k = 1 + 1/v
+def log_evidence(X_full, y, effective_indices, v=0.5, k=3, standardize=False):
     N = len(y)
     p = len(effective_indices)
 
     if standardize:
-        y = (y - y.mean())/y.std()
+        k = 1 + 1/v
+        X_full = (X_full-X_full.mean(axis=0))/X_full.std(axis=0)
+        y = (y-y.mean())/y.std()
+
     K = X_full[:, effective_indices]
 
     KT = K.T # intermediate var
@@ -23,7 +25,8 @@ def log_evidence(X_full, y, effective_indices, v=1, standardize=False):
     A = KT@K + Sigma
     A_inv = np.linalg.pinv(A)
     b = KTy + Smu
-    xi = (yTy + muT@Smu - b.T@(A_inv@b))[0][0]
+    xi = yTy + muT@Smu - b.T@(A_inv@b)
+    xi = xi[0][0]
     
     return N*((np.linalg.slogdet(Sigma)[1] - np.linalg.slogdet(A)[1])/(2*N) - 0.5*np.log(2*np.pi) - \
               (0.5 + k/N)*np.log(xi/2 + 1/v) - (k*np.log(v))/N + (loggamma(N/2 + k) - loggamma(k))/N)
